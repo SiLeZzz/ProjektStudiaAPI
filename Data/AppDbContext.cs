529cc9ff8790c49971a0ba3c7cb2fd4eb03e41ff
Szymon <Szymon@LAPTOP-19UQCT13>
2026-04-19 21:14:15 +0200

    Dodaj pliki projektów.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
